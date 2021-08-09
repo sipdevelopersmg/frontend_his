f3a1d9ca7387be00402a784b5d499065527424a2
scarletviper8 <scarletviper8@gmail.com>
2021-08-09 11:33:31 +0700

    UPDATE PIS ROUTING MODULE
    UPDATE PIS ROUTING MODULE

--- src/app/modules/PIS/pis-routing.module.ts
+++ src/app/modules/PIS/pis-routing.module.ts
@@ -18,8 +18,8 @@ const pisRoutes: Routes = [
     {
         path: "setup-data", component: null, data: { title: "Setup Data" },
         children: [
-            {path: "setup-etnis", component: SetupEtnisComponent, data: { title: "Penerimaan" }},
-            {path: "setup-bahasa", component: SetupBahasaComponent, data: { title: "Setup Bahasa" }},
+            { path: "setup-etnis", component: SetupEtnisComponent, data: { title: "Penerimaan" } },
+            { path: "setup-bahasa", component: SetupBahasaComponent, data: { title: "Setup Bahasa" } },
             {
                 path: "setup-tarif", component: null, data: { title: "Setup Tarif" },
                 children: [
@@ -37,10 +37,10 @@ const pisRoutes: Routes = [
     {
         path: "IRJA", component: null, data: { title: "IRJA" },
         children: [
-            { path: "pendaftaran-pasien-baru", component: PendaftaranPasienBaruComponent, data: { title: "Pendaftaran Pasien Baru" }, },
+            { path: "pendaftaran-pasien-baru", component: PendaftaranPasienBaruComponent, data: { title: "Pendaftaran Pasien Baru Rawat Jalan" }, },
             { path: "antrian-pasien-rawat-jalan", component: AntrianPasienRawatJalanComponent, data: { title: "Antrian Pasien Rawat Jalan" }, },
-            { path: "pendaftaran-ulang-pasien", component: DaftarUlangPasienComponent, data: { title: "Pendaftaran Ulang Pasien" }, },
-            { path: "daftar-pasien", component: DaftarPasienComponent, data: { title: "Pendaftaran Pasien Baru" }, },
+            { path: "pendaftaran-ulang-pasien", component: DaftarUlangPasienComponent, data: { title: "Pendaftaran Ulang Pasien Rawat Jalan" }, },
+            { path: "daftar-pasien", component: DaftarPasienComponent, data: { title: "Daftar Pasien Rawat Jalan" }, },
             { path: "pelayanan-pasien-irja", component: AdmisiPasienRawatJalanComponent, data: { title: "Pelayanan Pasien Rawat Jalan" }, },
         ]
     },
@@ -48,7 +48,7 @@ const pisRoutes: Routes = [
         path: "IRNA", component: null, data: { title: "IRNA" },
         children: [
             { path: "pelayanan-pasien-irna", component: PelayananPasienIrnaComponent, data: { title: "Pelayanan Pasien Rawat Inap" } },
-            { path: "pendaftaran-pasien-baru", component: PendaftaranPasienBaruIrnaComponent, data: { title: "Pendaftaran Pasien Baru" } }
+            { path: "pendaftaran-pasien-baru", component: PendaftaranPasienBaruIrnaComponent, data: { title: "Pendaftaran Pasien Baru Rawat Inap" } }
         ]
     }
 ]
