8364c6475049c602a24b5a0f0d51794228ffbc25
Unknown <triastartya@gmail.com>
2021-08-09 11:18:04 +0700

    setup oke

--- src/app/modules/MM/pages/setup-data/setup-satuan/setup-satuan.component.ts
+++ src/app/modules/MM/pages/setup-data/setup-satuan/setup-satuan.component.ts
@@ -78,11 +78,6 @@ export class SetupSatuanComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.ButtonNav = [
-            { Id: 'SaveAndNew', Captions: 'Save', Icons1: 'fa-save' },
-            { Id: 'Clear', Captions: 'Clear', Icons1: 'fa-redo-alt' },
-            { Id: 'Cancel', Captions: 'Back', Icons1: 'fa-arrow-left' },
-        ];
 
         this.GridDataToolbar = [
             { text: 'Add', tooltipText: 'Add', prefixIcon: 'fas fa-plus fa-sm', id: 'add' },
@@ -171,6 +166,11 @@ export class SetupSatuanComponent implements OnInit {
         this.inputFieldState = 'normal';
         this.FormInputData.reset();
         this.StatusFormNew = true;
+        this.ButtonNav = [
+            { Id: 'SaveAndNew', Captions: 'Save', Icons1: 'fa-save' },
+            { Id: 'Clear', Captions: 'Clear', Icons1: 'fa-redo-alt' },
+            { Id: 'Cancel', Captions: 'Back', Icons1: 'fa-arrow-left' },
+        ];
     };
 
     /** method setting edit data */
@@ -179,6 +179,10 @@ export class SetupSatuanComponent implements OnInit {
         this.SetFrom(this.SelectedData);
         this.StatusFormNew = false;
         this.OrgTabsRef.onNavigateTabUsingTabId(1, 'Input');
+        this.ButtonNav = [
+            { Id: 'SaveAndNew', Captions: 'Save', Icons1: 'fa-save' },
+            { Id: 'Cancel', Captions: 'Back', Icons1: 'fa-arrow-left' },
+        ];
 
     };
 
@@ -187,6 +191,9 @@ export class SetupSatuanComponent implements OnInit {
         this.OrgTabsRef.onNavigateTabUsingTabId(1, 'Input');
         this.inputFieldState = 'detail';
         this.SetFrom(this.SelectedData);
+        this.ButtonNav = [
+            { Id: 'Cancel', Captions: 'Back', Icons1: 'fa-arrow-left' },
+        ];
     }
 
     /** Method untuk mengkosongkan data yang ada di form*/
