798c808890e578ddb78d29ccb158f90f1350314c
scarletviper8 <scarletviper8@gmail.com>
2021-08-19 10:56:39 +0700

    UPDATE ENVIRONMENT PRODUCTION
    Penambahan Endpoint PharMM pada Environment Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,11 @@ export const environment = {
     firebaseRtdbUrl: "https://dashboard-template-2708-default-rtdb.asia-southeast1.firebasedatabase.app/",
     api: "http://68.183.180.59:2022/api/",
     webApiDo: "http://174.138.22.139:5001/api",
-    webApiPis: "http://174.138.22.139:5002/api/",
+    webApiMM: "http://174.138.22.139:5001/api/",
+
+    /** 
+     * @webApiPis http://174.138.22.139:8888/api/pis/
+     * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /pis 
+    */
+    webApiPis: "http://174.138.22.139:8888/api/pis/",
 };
