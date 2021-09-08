75ec6d47a63960e1321d96ccdf24b50a66c6e74d
scarletviper8 <scarletviper8@gmail.com>
2021-09-08 08:52:11 +0700

    UPLOAD ENV BILLING
    UPLOAD ENV BILLING

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
     api: "http://68.183.180.59:2022/api/",
     webApiDo: "http://174.138.22.139:5001/api",
     webApiMM: "http://174.138.22.139:8888/api/pharmm/",
+    webApiPHARMACY: "http://174.138.22.139:8888/api/farmasi/",
 
     /** 
      * @webApiPis http://174.138.22.139:8888/api/pis/
@@ -13,8 +14,8 @@ export const environment = {
     webApiPis: "http://174.138.22.139:8888/api/pis/",
 
     /** 
-     * @webApiBilling http://174.138.22.139:8888/api/pis/
-     * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /pis 
+     * @webApiBilling http://174.138.22.139:8888/api/billing/
+     * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /billing 
     */
     webApiBilling: "http://174.138.22.139:8888/api/billing/",
 };
