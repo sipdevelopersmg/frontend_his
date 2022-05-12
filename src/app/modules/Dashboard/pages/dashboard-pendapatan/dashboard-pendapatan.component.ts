aafa2e330dd5651393a8e11af4f2e7bb37281d21
faturgautama.dev <faturgautama.dev@gmail.com>
2022-05-12 17:23:11 +0700

    UPDATE
    UPDATE

--- src/app/modules/Dashboard/pages/dashboard-pendapatan/dashboard-pendapatan.component.ts
+++ src/app/modules/Dashboard/pages/dashboard-pendapatan/dashboard-pendapatan.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard-pendapatan',
-  templateUrl: './dashboard-pendapatan.component.html',
-  styleUrls: ['./dashboard-pendapatan.component.css']
+    selector: 'app-dashboard-pendapatan',
+    templateUrl: './dashboard-pendapatan.component.html',
+    styleUrls: ['./dashboard-pendapatan.component.css']
 })
 export class DashboardPendapatanComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
