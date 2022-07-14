b0b7e8bbe998666af0233c44d8d2146babd434ea
faturgautama.dev <faturgautama.dev@gmail.com>
2022-07-14 15:24:05 +0700

    TES

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -19,46 +19,46 @@ export const environment = {
     version: 'DEV',
 
     // ** Web Api 
-    webApiForSentry: `${webApi}:8888/api`,
+    webApiForSentry: `${apiUrl}:8888/api`,
 
     /** 
      * @webApiMM http://174.138.22.139:8888/api/pharmm/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /pharmm 
     */
-    webApiMM: `${webApi}:8888/api/pharmm/`,
+    webApiMM: `${apiUrl}:8888/api/pharmm/`,
 
     /** 
      * @webApiPHARMACY http://174.138.22.139:8888/api/farmasi/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /farmasi 
     */
-    webApiPHARMACY: `${webApi}:8888/api/farmasi/`,
+    webApiPHARMACY: `${apiUrl}:8888/api/farmasi/`,
 
     /** 
      * @webApiPis http://174.138.22.139:8888/api/pis/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /pis 
     */
-    webApiPis: `${webApi}:8888/api/pis/`,
+    webApiPis: `${apiUrl}:8888/api/pis/`,
 
     /** 
      * @webApiBilling http://174.138.22.139:8888/api/billing/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /billing 
     */
-    webApiBilling: `${webApi}:8888/api/billing/`,
+    webApiBilling: `${apiUrl}:8888/api/billing/`,
 
     /** 
      * @webApiAdmisi http://174.138.22.139:8888/api/admisi/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /admisi 
     */
-    webApiAdmisi: `${webApi}:8888/api/admisi/`,
+    webApiAdmisi: `${apiUrl}:8888/api/admisi/`,
 
     /** 
      * @webApiLaporan http://174.138.22.139:8888/api/billing/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /billing 
     */
-    webApiLaporan: `${webApi}:8080/jasperserver/rest_v2/reports/`,
+    webApiLaporan: `${apiUrl}:8080/jasperserver/rest_v2/reports/`,
 
     /** 
         * @webApiSocket http://174.138.22.139:3000
     */
-    webApiSocket: `${webApi}:3000`,
+    webApiSocket: `${apiUrl}:3000`,
 };
