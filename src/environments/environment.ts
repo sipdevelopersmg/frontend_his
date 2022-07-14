98d5869ac9b9c21a670c26c72ad4191bf8e6d20f
faturgautama.dev <faturgautama.dev@gmail.com>
2022-07-14 15:55:55 +0700

    TES

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -22,48 +22,48 @@ export const environment = {
     version: '0.0.0',
 
     // ** Web Api 
-    webApiForSentry: `${apiUrl}:8888/api`,
+    webApiForSentry: `http://${apiUrl}:8888/api`,
 
     /** 
      * @webApiMM http://174.138.22.139:8888/api/pharmm/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /pharmm 
     */
-    webApiMM: `${apiUrl}:8888/api/pharmm/`,
+    webApiMM: `http://${apiUrl}:8888/api/pharmm/`,
 
     /** 
      * @webApiPHARMACY http://174.138.22.139:8888/api/farmasi/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /farmasi 
     */
-    webApiPHARMACY: `${apiUrl}:8888/api/farmasi/`,
+    webApiPHARMACY: `http://${apiUrl}:8888/api/farmasi/`,
 
     /** 
      * @webApiPis http://174.138.22.139:8888/api/pis/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /pis 
     */
-    webApiPis: `${apiUrl}:8888/api/pis/`,
+    webApiPis: `http://${apiUrl}:8888/api/pis/`,
 
     /** 
      * @webApiBilling http://174.138.22.139:8888/api/billing/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /billing 
     */
-    webApiBilling: `${apiUrl}:8888/api/billing/`,
+    webApiBilling: `http://${apiUrl}:8888/api/billing/`,
 
     /** 
      * @webApiAdmisi http://174.138.22.139:8888/api/admisi/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /admisi 
     */
-    webApiAdmisi: `${apiUrl}:8888/api/admisi/`,
+    webApiAdmisi: `http://${apiUrl}:8888/api/admisi/`,
 
     /** 
      * @webApiLaporan http://174.138.22.139:8888/api/billing/
      * @Keterangan Menggunakan Gateway jadi Port nya diubah ke 8888 dan ditambahkan /billing 
     */
-    webApiLaporan: `${apiUrl}:8080/jasperserver/rest_v2/reports/`,
+    webApiLaporan: `http://${apiUrl}:8080/jasperserver/rest_v2/reports/`,
 
     /** 
      * @webApiSocket http://174.138.22.139:3000
     */
-    webApiSocket: `${apiUrl}:3000`,
+    webApiSocket: `http://${apiUrl}:3000`,
 };
 
 /*
